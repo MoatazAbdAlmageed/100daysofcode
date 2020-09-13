5df26e35ed91d3804296b56977fb096e6759544a
MoatazAbdAlmageed <moataz@wuilt.com>
2020-09-13 06:24:19 +0200

    init

---
+++ 01javascript/index.js
@@ -0,0 +1,46 @@
+console.log("🚀🚀🚀🚀🚀🚀 100 days of javascript");
+
+const courses = {
+    pluralsight: {
+        "todo": [
+            "01 JavaScript- Getting Started/",
+            "02 JavaScript Syntax and Operators/",
+            "03 JavaScript Variables and Types/",
+            "04 JavaScript Arrays and Collections/",
+            "05 Javascript Generators and Iterators/",
+            "06 Working with JavaScript Modules/",
+            "07 JavaScript Objects, Prototypes, and Classes/",
+            "08 JavaScript- Functions/",
+            "09 JavaScript Promises and Async Programming/",
+            "Building a JavaScript Development Environment/",
+            "Creating a Living Style Guide with Sass and Vanilla JavaScript/",
+            "Human Readable JavaScript- CodeMash/",
+            "JavaScript Build Automation With Gulp.js/",
+
+        ],
+        "Done": [
+
+        ]
+    },
+    Others: {
+        "todo": [
+            "https://coderbyte.com/starter-course/learn-javascript-in-one-week",
+        ],
+        "Done": [
+
+        ],
+    },
+};
+
+/**
+ * Functions
+ */
+
+function getCourses() {
+    return courses;
+}
+
+console.log('🚀🚀🚀🚀🚀🚀 getCourses()');
+console.log(getCourses());
+console.log('----------------------------------------------------');
+console.log();
